658ea0166c1f8f246500d85e5609f80bdb474183
espinood <espinood@rose-hulman.edu>
2020-05-02 21:34:27 -0400

    Finished Create using AlertController (aka Dialog)

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -24,7 +24,36 @@ class MovieQuotesTableViewController: UITableViewController {
     }
     
     @objc func showAddQuoteDialog(){
-        print("You pressed the add button")
+        let alertController = UIAlertController(title: "Create a new movie quote",
+                                                message: "",
+                                                preferredStyle: .alert)
+        // Configure
+        alertController.addTextField{ (textField) in
+            textField.placeholder = "Quote"
+        }
+        
+        alertController.addTextField{ (textField) in
+            textField.placeholder = "Movie"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: nil)
+        alertController.addAction(cancelAction)
+        alertController.addAction(UIAlertAction(title: "Create Quote",
+                                                style: .default) { (action) in
+                                                    let quoteTextField = alertController.textFields![0] as UITextField
+                                                    let movieTextField = alertController.textFields![1] as UITextField
+                                                    print(quoteTextField.text!)
+                                                    print(movieTextField.text!)
+                                                    
+                                                    let newMovieQuote = MovieQuote(quote: quoteTextField.text!, movie:  movieTextField.text!)
+                                                    self.movieQuotes.insert(newMovieQuote, at: 0)
+                                                    self.tableView.reloadData()
+        })
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
